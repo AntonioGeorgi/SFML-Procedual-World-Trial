b4c9003c4e304eeb8622f57d8ff7162f7b8dc211
AntonioGeorgi <100966723+AntonioGeorgi@users.noreply.github.com>
2024-05-20 17:26:35 +0200

    Add files via upload

---
+++ Headers/Constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+constexpr int WIDTH_TILES  = 21;
+constexpr int HEIGHT_TILES = 21;
+
+constexpr int TILE_SIZE = 50;
+
+//f�r super efficienten Code suche: zero overhat progamming
+
+#endif
