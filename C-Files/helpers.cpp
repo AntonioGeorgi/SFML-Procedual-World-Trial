4c70e7adb727e0083deeddf1efcf6c3ce24e4349
Antonio Georgi <antonio.georgiive.de>
2024-05-21 17:48:35 +0200

    Finished the Backround and now only need to test it, then I am gonna write some actual comments.

--- C-Files/helpers.cpp
+++ C-Files/helpers.cpp
@@ -1,26 +1,15 @@
 #include "..\Headers\helpers.h"
 
-
-void Backround::update(std::vector<tile> tiles, unsigned short new_height = height, unsigned short new_tile_size = tile_size) {
-
-    if ((new_height != height) || (new_tile_size != tile_size))
-    {
-        height      = new_height;
-        tile_size   = new_tile_size;
-    }
-    
+void Backround::update(std::vector<tile> tiles) {    
     //get the width
-    unsigned short width = tiles.size/new_height;
+    unsigned short width = tiles.size()/height;
 
     //set nummber of vertices in array (6 per tile)
-    array_vertices.resize(tiles.size * height * 6);
+    array_vertices.resize(tiles.size() * height * 6);
 
     //loop through all tiles
     for (int i = 0; i < width; ++i)
         for (int j = 0; j < height; ++j) {
-            //get the current tile_type
-            int tile_type = tile_grid[i + j * width].state;
-
             // get a pointer to the triangles vertices of the current tile
             sf::Vertex* triangles = &array_vertices[(i + j * width) * 6];
 
@@ -32,15 +21,20 @@ void Backround::update(std::vector<tile> tiles, unsigned short new_height = heig
             triangles[4].position = sf::Vector2f((i + 1) * tile_size, j * tile_size);
             triangles[5].position = sf::Vector2f((i + 1) * tile_size, (j + 1) * tile_size);
 
-            // set matching coordinates in tilemap_texture
-            triangles[0].texCoords = sf::Vector2f(tile_position_width * tile_size, tile_position_height * tile_size);
-            triangles[1].texCoords = sf::Vector2f((tile_position_width + 1) * tile_size, tile_position_height * tile_size);
-            triangles[2].texCoords = sf::Vector2f(tile_position_width * tile_size, (tile_position_height + 1) * tile_size);
-            triangles[3].texCoords = sf::Vector2f(tile_position_width * tile_size, (tile_position_height + 1) * tile_size);
-            triangles[4].texCoords = sf::Vector2f((tile_position_width + 1) * tile_size, tile_position_height * tile_size);
-            triangles[5].texCoords = sf::Vector2f((tile_position_width + 1) * tile_size, (tile_position_height + 1) * tile_size);
+            // set color rgb values depending on the tiles stats
+            for (size_t i = 0; i < 6; i++)
+            {
+                triangles[i].color.r = show_tempature * tiles[j * width + i].tempature;
+                triangles[i].color.g = show_height * tiles[j * width + i].height;
+                triangles[i].color.b = show_humidity * tiles[j * width + i].humidity;
+            }
+        }   
+}
 
-        }
+void Backround::update(std::vector<tile> tiles, unsigned short new_height, unsigned short new_tile_size) {
+    height      = new_height;
+    tile_size   = new_tile_size;
+    update(tiles);
 }
 
 void Backround::draw(sf::RenderTarget& target, sf::RenderStates states) const
--- Headers/helpers.h
+++ Headers/helpers.h
@@ -12,18 +12,25 @@ struct tile
 };
 
 class Backround : public sf::Drawable, public sf::Transformable
+    /* 
+    Backround sets the color depending on the inputed tiles stats, 
+    at the moment can only show tile stats from 0 till 256S
+    */
 {
 public:
-	//Backround(unsigned short height, unsigned short tile_size);
-	//unsigned short give_height();
+	bool show_height 	= 1;
+	bool show_tempature = 1;
+	bool show_humidity	= 1;
+	
+	Backround(unsigned short height, unsigned short tile_size);
 
 private:
 	unsigned short tile_size;
 	unsigned short height;
 
 	sf::VertexArray array_vertices;
-	
-    void update(std::vector<tile> tiles, unsigned short new_height = height, unsigned short new_tile_size = tile_size);
+	void update(std::vector<tile> tiles);
+    void update(std::vector<tile> tiles, unsigned short new_height, unsigned short new_tile_size);
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 #endif
